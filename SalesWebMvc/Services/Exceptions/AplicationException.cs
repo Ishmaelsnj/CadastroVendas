555f95b1cd925bd74bf482522886b6fc8d49d502
Ishmael Junior <ishmaelsan@gmail.com>
2025-12-08 22:48:14 -0300

    Returning custom error page

---
+++ SalesWebMvc/Services/Exceptions/AplicationException.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class AplicationException
+    {
+        private string message;
+
+        public AplicationException(string message)
+        {
+            this.message = message;
+        }
+    }
+}
